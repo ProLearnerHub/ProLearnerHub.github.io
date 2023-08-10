2efeec7f1e97ebb613945b7ee575537eb7302237
Claud Watari <claudwatari95@gmail.com>
2023-08-10 09:14:28 +0300

    chore: start media styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 window.addEventListener('load', () => {
-// everything here
+    // everything here
 
-const toInsertYear = document.querySelector("#year");
-const year = new Date().getFullYear();
+    const toInsertYear = document.querySelector("#year");
+    const year = new Date().getFullYear();
 
-toInsertYear.textContent = year;
+    toInsertYear.textContent = year;
+
+    const checkMedia = () => {
+        if(window.innerWidth < 769) {
+            const flex = document.querySelectorAll(".flex")[0];
+            const classesToRemove = ["flex", "flex-column"]
+            flex ? flex.classList.remove(...classesToRemove) : null;
+        }
+    };
+    checkMedia();
+    window.addEventListener('resize', checkMedia);
 });
